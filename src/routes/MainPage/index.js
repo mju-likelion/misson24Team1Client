19ab54fe4f3274e40563bb0e65af64328714b13a
Low-profile <maxandy@naver.com>
2022-08-28 08:17:05 +0900

    feat: axios 추가

--- src/routes/MainPage/index.js
+++ src/routes/MainPage/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useState } from 'react';
 import SemiCircleProgressBar from 'react-progressbar-semicircle';
 
 import { ChangeButton } from '../../assets/svgs';
@@ -5,40 +7,71 @@ import Slide from '../../components/Slide/subwayInfo.js';
 import Logo from '../../image/Logo.png';
 import styles from './mainPage.module.scss';
 
-const MainPage = () => (
-  <div className={styles.wrapper}>
-    <div className={styles.header}>
-      <img src={Logo} className={styles.Logo} />
-      <div className={styles.title}>지금출발해</div>
-    </div>
-    <div className={styles.body}>
-      <div className={styles.progressBar}>
-        <SemiCircleProgressBar percentage={33} showPercentValue />
-      </div>
-      <div className={styles.inputForm}>
-        <div>
-          <input className={styles.inputStation} placeholder="출발역" />
+const MainPage = () => {
+    const [isChange, setIsChange] = useState(true);
+    const [startPoint, setStartPoint] = useState('');
+    const [endPoint, setEndPoint] = useState('');
+
+    const startOnChange = (e) => {
+        setStartPoint(e.target.value);
+    }
+
+    const endOnChange = (e) => {
+        setEndPoint(e.target.value);
+    }
+
+    const onClick = () => {
+        setIsChange(!isChange)
+    }
+
+    const sendRequest = async () => {
+        console.log(startPoint)
+        try {
+            const req = await axios.get('http://52.79.165.184/subway', {
+                departure: startPoint
+            });
+            console.log('성공', req.data)
+            return req.data;
+        } catch (e) {
+            console.log('실패', e)
+        }
+    }
+
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.header}>
+                <img src={Logo} className={styles.Logo} />
+                <div className={styles.title}>지금출발해</div>
+            </div>
+            <div className={styles.body}>
+                <div className={styles.progressBar}>
+                    <SemiCircleProgressBar percentage={33} showPercentValue />
+                </div>
+                <div className={styles.inputForm}>
+                    <div>
+                        {isChange ? <input className={styles.inputStation} placeholder="출발역" onChange={startOnChange} value={startPoint} /> : <input className={styles.inputStation} placeholder="도착역" onChange={endOnChange} value={endPoint} />}
+                    </div>
+                    <ChangeButton onClick={onClick} />
+                    <div>
+                        {isChange ? <input className={styles.inputStation} placeholder="도착역" onChange={endOnChange} value={endPoint} /> : <input className={styles.inputStation} placeholder="출발역" onChange={startOnChange} value={startPoint} />}
+                    </div>
+                </div>
+                <div className={styles.type}>
+                    <input type="checkbox" id="scales1" check={true} />
+                    <label htmlFor="scales1">10분전</label>
+                    <input type="checkbox" id="scales2" check={true} />
+                    <label htmlFor="scales2">5분전</label>
+                    <input type="checkbox" id="scales3" check={true} />
+                    <label htmlFor="scales3">3분전</label>
+                </div>
+                <button className={styles.lookUpButton} type="button" onClick={sendRequest}>
+                    조회하기
+                </button>
+                {/* <div className={styles.infoDiv}>정보</div> */}
+                <Slide />
+            </div>
         </div>
-        <ChangeButton />
-        <div>
-          <input className={styles.inputStation} placeholder="도착역" />
-        </div>
-      </div>
-      <div className={styles.type}>
-        <input type="checkbox" id="scales1" check={true} />
-        <label htmlFor="scales1">10분전</label>
-        <input type="checkbox" id="scales2" check={true} />
-        <label htmlFor="scales2">5분전</label>
-        <input type="checkbox" id="scales3" check={true} />
-        <label htmlFor="scales3">3분전</label>
-      </div>
-      <button className={styles.lookUpButton} type="button">
-        조회하기
-      </button>
-      {/* <div className={styles.infoDiv}>정보</div> */}
-      <Slide />
-    </div>
-  </div>
-);
+    )
+};
 
 export default MainPage;
